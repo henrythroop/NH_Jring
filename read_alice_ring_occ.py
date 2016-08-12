f4bec7cf04330a3b44d06865923c12a27cdd608c
Henry Throop <henry.throop@gmail.com>
2016-08-12 16:23:07 +0530

    Improvements to Alice ring occ search code.

--- read_alice_ring_occ.py
+++ read_alice_ring_occ.py
@@ -65,15 +65,25 @@ import hbt
 
 ####
 
-#sequence = 'O_RING_Occ3'
-sequence = 'O_Ring_Occ2'
+sequence 	= 'O_RING_Occ3'
+
+#   sequence 	= 'O_Ring_Occ2'
+
+fs           	= 15		# Font size
 
 dir_images = '/Users/throop/Data/NH_Alice_Ring/' + sequence + '/data/pluto/level2/ali/all'
+file_tm = '/Users/throop/gv/dev/gv_kernels_new_horizons.txt'
+
+cspice.furnsh(file_tm)
+
+#########
+# Read the Alice data
+#########
 
 file_list = glob.glob(dir_images + '/*fit')
 
 #file_list = file_list[0:10]
-t_all = []
+met_all = []  # A long array with a list of all of the timestamps
 count_rate_all = []
 
 for file in file_list:
@@ -81,34 +91,102 @@ for file in file_list:
 #    print "."
     startmet = data['header_spect']['STARTMET']
     dt = data['header_count_rate']['SAMPLINT']
-    count_rate = data['count_rate']
+    count_rate_i = data['count_rate']              # Extract count rate from file
+
+    met_i = startmet + dt * hbt.frange(0, np.shape(count_rate_i)[0]-1)   # Create the MET by interpolation
+
+    print "Read file " + os.path.basename(file) + ", MET " + repr(startmet) + ' = ' + hbt.met2utc(startmet) + \
+      ', N = ' + repr(len(count_rate_i)) + ' samples' + \
+      ', duration = ' + repr(dt * len(count_rate_i)) + ' sec'
     
-    t = startmet + dt * hbt.frange(0, np.shape(count_rate)[0]-1)
-    
-    print "Read file " + file + ", MET = " + repr(startmet) + ', N = ' + repr(len(count_rate))
-    
-    t_all.append(t)
-    count_rate_all.append(count_rate)
+    met_all.append(met_i)
+    count_rate_all.append(count_rate_i)
 
-count_rate_flat = np.array([item for sublist in count_rate_all for item in sublist])
-t_flat =          np.array([item for sublist in t_all for item in sublist])
+count_rate  = np.array([item for sublist in count_rate_all for item in sublist])  # Flatten the count rate (from 2D, to 1D)
+met         = np.array([item for sublist in met_all for item in sublist])         # Flatten the MET array  (from 2D, to 1D)
 
-# Remove the last few samples
+# Compute ET for each timestep
 
-count_rate_flat = count_rate_flat
+et_start = cspice.utc2et(hbt.met2utc(startmet))
+et       = met - met[0] + et_start    # ET is now fully populated and correct
 
-#plt.plot(t_flat, count_rate_flat, marker = '.', linestyle='none')
+t        = et - et[0]                 # Seconds since start of observation
 
-binning = 1000
-fs = 15
-count_rate_s = hbt.smooth_boxcar(count_rate_flat, binning)[binning:-binning]
-t_s = t_flat[binning:-binning]
+# Computed a smoothed count rate. _s indicates smoothed. Array is cropped at edges too.
 
-plt.plot(t_s - t_s[0], count_rate_s, marker = '.', linestyle='none', ms=0.1)
+binning      = 25000		# Smoothing 
+count_rate_s = hbt.smooth_boxcar(count_rate, binning)[binning:-binning]
+
+# Compute truncated versions of the time arrays, just in case they are useful
+
+t_s          = t[binning:-binning]
+et_s         = et[binning:-binning]
+met_s        = met[binning:-binning]
+
+##########
+# Compute the angle from the star to Alice boresight, for every timestep.
+##########
+
+# NB: For FSS, I got the FSS-Sun angle directly from Gabe -- I didn't get it from SPICE.
+
+# Get vector to star. 67 Ori = HR 2159 = HD 41753, a V=4.2 B3V. RA=91.89, Dec=14.77.
+
+ra_star  = 91.89 * hbt.d2r
+dec_star = 14.77 * hbt.d2r
+
+vec_star_j2k = cspice.radrec(1., ra_star, dec_star)
+
+name_fov = 'NH_ALICE_AIRGLOW'
+
+vec_bsight_alice = (-1, 0, 0)  # -X defines Alice SOC FOV
+
+vsep = np.zeros(np.size(et))
+
+for i,et_i in enumerate(et):
+  mx = cspice.pxform(name_fov, 'J2000', et[i])
+  vec_alice_j2k = cspice.mxvg(mx, vec_bsight_alice)		# ICY did not have mxvg(), but pdstools does.
+  vsep[i] = cspice.vsep(vec_star_j2k, vec_alice_j2k)   # Angular separation, in radians
+
+#plt.plot(t, count_rate, marker = '.', linestyle='none')
+
+##########
+# Make plots
+##########
+
+plt.rcParams['figure.figsize'] = 15,5
+
+# Plot of count rate vs. time
+
+plt.plot(t_s, count_rate_s, marker = '.', linestyle='none', ms=0.1)
 plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
 plt.ylabel('Count Rate', fontsize=fs)
 plt.xlabel('t since start [sec]', fontsize=fs)
+plt.show()
 
+# Plot of pointing vs. time
 
-stop
-   
+#   plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d - 0.65 - ((et-et[0] - 1900) / 1000))
+plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d)
+#   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
+plt.xlabel('Seconds', fontsize=fs)
+plt.ylabel('Degrees from star', fontsize=fs)
+plt.show()
+
+##########
+# Do another plot of pointing v. time
+#########
+
+plt.plot(t_s, count_rate_s, marker = '.', linestyle='none', ms=0.1)
+plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
+plt.ylabel('Count Rate', fontsize=fs)
+plt.xlim((0,210))
+plt.show()
+
+plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d)
+plt.xlim((0,210))
+plt.ylim((0,0.04))
+#   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
+plt.xlabel('Seconds', fontsize=fs)
+plt.ylabel('Degrees from star', fontsize=fs)
+plt.show()
+
