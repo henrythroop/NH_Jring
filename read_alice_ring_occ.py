f837b31a321954ed2a4aa45062d6ac56290ded9c
Henry Throop <henry.throop@gmail.com>
2016-08-17 21:51:58 +0530

    Alice ring development.

--- read_alice_ring_occ.py
+++ read_alice_ring_occ.py
@@ -68,7 +68,7 @@ import hbt
 ##########
 # (NB: The spellling / capitalization is inconsistent in SAPNAME vs. VISITNAM. I have standardized it here.)
 
-#   sequence 	= 'O_RING_OC3'
+#sequence 	= 'O_RING_OC3'
 sequence 	= 'O_RING_OC2'
 
 fs           	= 15		# Font size
@@ -102,7 +102,7 @@ for file in file_list:
 
     met_i = startmet + dt * hbt.frange(0, np.shape(count_rate_i)[0]-1)   # Create the MET by interpolation
 
-    print "Read file " + os.path.basename(file) + ", MET " + repr(startmet) + ' = ' + hbt.met2utc(startmet) + \
+    print "Read " + os.path.basename(file) + ", MET " + repr(startmet) + ' = ' + hbt.met2utc(startmet) + \
       ', N = ' + repr(len(count_rate_i)) + ' samples' + \
       ', duration = ' + hbt.trunc(dt * len(count_rate_i),3) + ' sec'
 #       print "  " + visitnam + " " + spcutcal
@@ -128,7 +128,10 @@ num_dt   = np.size(et)
 
 # Computed a smoothed count rate. _s indicates smoothed. Array is cropped at edges too.
 
-binning      = 25000		# Smoothing 
+binning      = 3000		# Smoothing. 25000 is too much (shows opposite trend!). 5000 and 1000 look roughly similar.
+                            # To be meaningful, the binning timescale must be less than the deadband timescale (~20-30 sec RT).
+                            # At binning=3000, it's 12 sec... so that is the longest we'd really want to go.
+                            
 count_rate_s = hbt.smooth_boxcar(count_rate, binning)[binning:-binning]
 
 # Compute truncated versions of the time arrays, just in case they are useful
@@ -202,28 +205,32 @@ plt.show()
 
 #   plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d - 0.65 - ((et-et[0] - 1900) / 1000))
 plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d)
-#   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
+#   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + \
+#  repr(dt * binning) + ' sec', fontsize=fs)
 plt.xlabel('Seconds since ' + utc_start, fontsize=fs)
 plt.ylabel('Degrees from star', fontsize=fs)
+#plt.xlim(0,500)
 plt.show()
 
 ##########
 # Do another plot of pointing v. time
 #########
 
-plt.plot(t_s, count_rate_s, marker = '.', linestyle='none', ms=0.1)
-plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
-plt.ylabel('Count Rate', fontsize=fs)
-plt.xlim((0,210))
-plt.show()
-
-plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d)
-plt.xlim((0,210))
-plt.ylim((0,0.04))
-#   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
-plt.xlabel('Seconds', fontsize=fs)
-plt.ylabel('Degrees from star', fontsize=fs)
-plt.show()
+#plt.plot(t_s, count_rate_s, marker = '.', linestyle='none', ms=0.1)
+#plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + ' = ' + \
+# repr(dt * binning) + ' sec', fontsize=fs)
+#plt.ylabel('Count Rate', fontsize=fs)
+#plt.xlim((0,210))
+#plt.show()
+#
+#plt.plot(et - et[0], (vsep % 0.02) * hbt.r2d)
+#plt.xlim((0,210))
+#plt.ylim((0,0.04))
+##   plt.title(sequence + ', dt = ' + repr(dt) + ' sec, binned x ' + repr(binning) + \
+#  ' = ' + repr(dt * binning) + ' sec', fontsize=fs)
+#plt.xlabel('Seconds', fontsize=fs)
+#plt.ylabel('Degrees from star', fontsize=fs)
+#plt.show()
 
 ##########
 # Make a plot of motion thru the deadband
@@ -234,6 +241,72 @@ plt.plot(ra*hbt.r2d, dec*hbt.r2d, linestyle='none', marker='.', ms=1)
 plt.title(sequence + ', start = ' + utc_start)
 plt.xlabel('RA [deg]')
 plt.ylabel('Dec [deg]')
+ax = plt.gca()
+ax.ticklabel_format(useOffset=False)  # Turn off the 'offset' that matplotlib can use
+plt.show()
+
+### Testing for pointing
+
+utc_test = '2015 jul 16 21:25:00'  # First timestep of OC3 movie
+et_test  = cspice.utc2et(utc_test)
+
+mx = cspice.pxform('NH_ALICE_AIRGLOW', 'J2000', et_test)
+vec_alice_j2k = cspice.mxvg(mx, vec_bsight_alice)		# ICY did not have mxvg(), but pdstools does.
+vsep_test = cspice.vsep(vec_star_j2k, vec_alice_j2k)   # Angular separation, in radians
+
+(junk, ra_test, dec_test) = cspice.recrad(vec_alice_j2k)
+
+## Now do some correlation between position and pointing, to see if there is anything I should unwrap there.
+#
+# Concl: there is quite a bit of trend across the detector. So, a lot of the variation I'm seeing
+# in DN value is probably not due to statistics, but due to sensitivity across the detector.
+
+# Now I need to make an XY scatter plot of all these data points. I really have no idea how to do this.
+
+ra_s = ra[binning:-binning]    # Same as RA, but cropped, so the edges are missing. Has same # elements as count_rate_s (smoothed)
+dec_s = dec[binning:-binning]
+
+plt.rcParams['figure.figsize'] = 10,10
+plt.plot(ra_s, count_rate_s, linestyle='none', marker='.', ms=0.1)
+plt.xlabel('RA [deg]', fontsize=fs)
+plt.title(sequence, fontsize=fs*1.5)
+ax = plt.gca()
+ax.ticklabel_format(useOffset=False)
+plt.ylabel('DN (binned ' + repr(binning) + ')', fontsize=fs)
+plt.show()
+
+plt.plot(dec_s, count_rate_s, linestyle='none', marker='.', ms=0.1)
+
+num_dx = 100
+num_dy = num_dx
+
+ra_arr  = np.linspace(np.min(ra),  np.max(ra),  num_dx)
+dec_arr = np.linspace(np.min(dec), np.max(dec), num_dy)
+
+count_rate_s_arr = griddata((ra_s, dec_s), count_rate_s, (ra_arr[None,:], dec_arr[:,None]), method='cubic')
+
+plt.imshow(count_rate_s_arr, interpolation='none', vmin=16.2,vmax=16.45)
+plt.imshow(count_rate_s_arr, interpolation='none', vmin=16.4,vmax=16.8)
+
+plt.title(sequence + ', Mean DN vs position, binning=' + repr(binning), fontsize=fs)
+plt.xlabel('RA bin', fontsize=fs)
+plt.ylabel('Dec bin', fontsize=fs)
+plt.colorbar()
+plt.show()
+
+# Same, but unbinned
+
+#count_rate_arr = griddata((ra, dec), count_rate, (ra_arr[None,:], dec_arr[:,None]), method='cubic')
+#
+#plt.imshow(count_rate_arr, interpolation='none', vmin=15, vmax=17)
+#plt.title(sequence + ', Mean DN vs position, binning=none', fontsize=fs)
+#plt.xlabel('RA bin', fontsize=fs)
+#plt.ylabel('Dec bin', fontsize=fs)
+#plt.colorbar()
+#plt.show()
 
 
+# 
 
+
+  
