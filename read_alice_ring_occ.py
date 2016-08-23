79a701d79b3100bf6e9c488c241c108fdffdea30
Henry Throop <henry.throop@gmail.com>
2016-08-23 09:47:38 +0530

    Improvements to Alice Occ analysis. Properly determine barycentric distance now.

--- read_alice_ring_occ.py
+++ read_alice_ring_occ.py
@@ -46,6 +46,9 @@ import wcsaxes
 import time
 from scipy.interpolate import griddata
 
+from mpl_toolkits.axes_grid1 import host_subplot # For adding a second axis to a plot
+import mpl_toolkits.axisartist as AA             # For adding a second axis to a plot
+
 import imreg_dft as ird
 import re # Regexp
 import pickle # For load/save
@@ -66,8 +69,8 @@ import hbt
 ##########
 # (NB: The spellling / capitalization is inconsistent in SAPNAME vs. VISITNAM. I have standardized it here.)
 
-#sequence 	= 'O_RING_OC3'
-sequence 	= 'O_RING_OC2'
+sequence 	= 'O_RING_OC3'
+#sequence 	= 'O_RING_OC2'
 
 
 binning      = 25000		# Smoothing. 25000 is too much (shows opposite trend!). 5000 and 1000 look roughly similar.
@@ -176,20 +179,27 @@ coeffs_dec = linregress(dec, count_rate)
 count_rate_nonlinear = coeffs_ra.intercept + coeffs_ra.slope * ra - np.mean(count_rate)
 count_rate_fixed = count_rate - count_rate_nonlinear 
 
-# Computed a smoothed count rate. _s indicates smoothed. Array is cropped at edges too.
+#==============================================================================
+# Smooth the data at several different binnings
+#==============================================================================
 
-                            
-count_rate_s = hbt.smooth_boxcar(count_rate, binning)[binning:-binning]
+count_rate_fixed_30000 = hbt.smooth_boxcar(count_rate_fixed, 30000)
+count_rate_fixed_3000 = hbt.smooth_boxcar(count_rate_fixed, 3000)
+count_rate_fixed_300 = hbt.smooth_boxcar(count_rate_fixed, 300)
+count_rate_fixed_30 = hbt.smooth_boxcar(count_rate_fixed, 30)
 
-count_rate_fake_s = hbt.smooth_boxcar(count_rate_fake, binning)[binning:-binning]
+count_rate_3000 = hbt.smooth_boxcar(count_rate, 3000)
+count_rate_30000 = hbt.smooth_boxcar(count_rate, 30000)
 
-count_rate_fixed_s = hbt.smooth_boxcar(count_rate_fixed, binning)[binning:-binning]
+count_rate_fake_3000 = hbt.smooth_boxcar(count_rate_fake, 3000)
+count_rate_fake_30000 = hbt.smooth_boxcar(count_rate_fake, 30000)
 
 # Compute truncated versions of the time arrays, just in case they are useful
+# _s extension = 'smoothed'
 
-t_s          = t[binning:-binning]
-et_s         = et[binning:-binning]
-met_s        = met[binning:-binning]
+#t_s          = t[binning:-binning]
+#et_s         = et[binning:-binning]
+#met_s        = met[binning:-binning]
 
 ##########
 # Calculate statistics
@@ -200,7 +210,6 @@ met_s        = met[binning:-binning]
 
 sigma_s = np.mean(count_rate) * np.sqrt(np.mean(count_rate * binning)) / (np.mean(count_rate * binning))
 
-
 #==============================================================================
 # Make plots
 #==============================================================================
@@ -209,32 +218,35 @@ plt.rcParams['figure.figsize'] = 15,5
 
 # Plot of count rate vs. time
 
+
 if (sequence == 'O_RING_OC3'):
     offset_fake = 0.15
 if (sequence == 'O_RING_OC2'):
     offset_fake = 0.15
     
-plt.plot(t_s, count_rate_s, marker = '.', linewidth=0.5, ms=0.1, label='Alice, Raw')
-plt.plot(t_s, count_rate_fixed_s, linewidth=0.5, ms=0.1, label='Alice, Linear Row Trend Removed')
-plt.plot(t_s, count_rate_fake_s - offset_fake, linewidth=0.5, ms=0.1, label='Fake Poisson Data')
+binning = 30000
+    
+plt.plot(t, count_rate_30000, marker = '.', linewidth=0.5, ms=0.1, label='Alice, Raw')
+plt.plot(t, count_rate_fixed_30000, linewidth=0.5, ms=0.1, label='Alice, Linear Row Trend Removed')
+plt.plot(t, count_rate_fake_30000 - offset_fake, linewidth=0.5, ms=0.1, label='Fake Poisson Data')
 
 plt.title(sequence + ', dt = ' + repr(dt) + ' sec, smoothed x ' + repr(binning) + ' = ' + repr(dt * binning) + ' sec' + \
                      ', 1$\sigma$ = ' + hbt.trunc(sigma_s,4), fontsize=fs)
 
-plt.errorbar(100, np.mean(count_rate_s) + 5 * sigma_s, xerr=binning*dt/2, yerr=None, label='Binning Width', linewidth=2) 
+plt.errorbar(100, np.mean(count_rate_3000) + 5 * sigma_s, xerr=binning*dt/2, yerr=None, label='Binning Width', linewidth=2) 
 			# X 'error bar' -- show the bin width
-plt.errorbar(300, np.mean(count_rate_s) + 5 * sigma_s, xerr=None, yerr=sigma_s/2, label='1$\sigma$ shot noise', linewidth=2) 
+plt.errorbar(300, np.mean(count_rate_3000) + 5 * sigma_s, xerr=None, yerr=sigma_s/2, label='1$\sigma$ shot noise', linewidth=2) 
 			# Y 'error bar' -- show the binned shot noise error
 plt.legend()
 plt.ylabel('Count Rate (smoothed)', fontsize=fs)
 plt.xlabel('Time since ' + utc_start + ' [sec]', fontsize=fs)
-plt.xlim((0, np.max(t_s)+50))
+plt.xlim((t[binning], t[-binning])) # Remove transients at edges
 
 if (sequence == 'O_RING_OC3'):
-  plt.ylim((np.mean(count_rate_s) -10*sigma_s, np.mean(count_rate_s) +11*sigma_s))
+  plt.ylim((np.mean(count_rate_3000) -10*sigma_s, np.mean(count_rate_3000) +11*sigma_s))
 
 if (sequence == 'O_RING_OC2'):
-  plt.ylim((np.mean(count_rate_s) -9*sigma_s, np.mean(count_rate_s) +11*sigma_s))
+  plt.ylim((np.mean(count_rate_3000) -9*sigma_s, np.mean(count_rate_3000) +11*sigma_s))
     
 plt.show()
 
@@ -265,20 +277,22 @@ plt.show()
 # Concl: there is quite a bit of trend across the detector. So, a lot of the variation I'm seeing
 # in DN value is probably not due to statistics, but due to sensitivity across the detector.
 
-ra_s = ra[binning:-binning]    # Same as RA, but cropped, so the edges are missing. Has same # elements as count_rate_s (smoothed)
-dec_s = dec[binning:-binning]
-
 #==============================================================================
 # Make a plot of RA vs DN. 
 # This is to see if there is a trend in brightness as we move from one side of slit to the other.
 #==============================================================================
 
+ra_s = ra[binning:-binning]    # Same as RA, but cropped, so the edges are missing. Has same # elements as count_rate_s (smoothed)
+dec_s = dec[binning:-binning]
+
+crop = 3000  # Don't plot quite at the start and end of array, to avoid transients
+
 plt.rcParams['figure.figsize'] = 16,8
 
 plt.subplot(1,2,1)
 plt.rcParams['figure.figsize'] = 10,10
-plt.plot(ra_s*hbt.r2d, count_rate_s, linestyle='none', marker='.', ms=0.1)
-plt.plot(ra*hbt.r2d, count_rate_nonlinear + np.mean(count_rate), color='red')
+plt.plot(ra[crop:-crop]*hbt.r2d, count_rate_3000[crop:-crop], linestyle='none', marker='.', ms=0.1)
+plt.plot(ra[crop:-crop]*hbt.r2d, count_rate_nonlinear[crop:-crop] + np.mean(count_rate), color='red') # Add linear fit on top
 plt.xlabel('RA [deg]', fontsize=fs)
 plt.title(sequence + ': DN vs. Position', fontsize=fs*1.5)
 ax = plt.gca()
@@ -286,9 +300,8 @@ ax.ticklabel_format(useOffset=False)
 plt.ylabel('DN (smoothed x ' + repr(binning) + ')', fontsize=fs)
 
 plt.subplot(1,2,2)
-
 plt.rcParams['figure.figsize'] = 10,10
-plt.plot(dec_s*hbt.r2d, count_rate_s, linestyle='none', marker='.', ms=0.1)
+plt.plot(dec[crop:-crop]*hbt.r2d, count_rate_3000[crop:-crop], linestyle='none', marker='.', ms=0.1)
 plt.xlabel('Dec [deg]', fontsize=fs)
 plt.title(sequence + ': DN vs. Position', fontsize=fs*1.5)
 ax = plt.gca()
@@ -311,7 +324,7 @@ num_dy = num_dx
 ra_arr  = np.linspace(np.min(ra),  np.max(ra),  num_dx)
 dec_arr = np.linspace(np.min(dec), np.max(dec), num_dy)
 
-count_rate_s_arr = griddata((ra_s, dec_s), count_rate_s, (ra_arr[None,:], dec_arr[:,None]), method='cubic')
+count_rate_s_arr = griddata((ra[crop:-crop], dec[crop:-crop]), count_rate_3000[crop:-crop], (ra_arr[None,:], dec_arr[:,None]), method='cubic')
 
 # Make the plot, scaled vertically as per the sequence read in
 
@@ -336,44 +349,45 @@ plt.show()
 # The orientation of IAU_PLUTO will change from start to end of observation, and this will give us a 
 # different answer in the end vs. start.
          
-et_start = np.min(et)
-et_end   = np.max(et) + duration[-1]
+#et_start = np.min(et)
+#et_end   = np.max(et) + duration[-1]
 
-for et_i in (et_start, et_end):
-    plane_plu = cspice.nvp2pl([0,0,1], [0,0,0])    # nvp2pl: Normal Vec + Point to Plane
+plane_plu = cspice.nvp2pl([0,0,1], [0,0,0])    # nvp2pl: Normal Vec + Point to Plane
+
+# Get vector from Pluto to star, in IAU_PLUTO. This will define the *vector* portion of the ray.
+
+vec_plu_star = cspice.radrec(1., ra_star, dec_star) # Vector from Pluto to star, in J2K
+
+# Set the values of ET for which we look up the projected radius. We could do every ET, but that's pretty slow, and the 
+# behavior is essentially linear. So, we do the opposite extreme, which is just the starting and ending values.
+
+et_vals = hbt.mm(et)
+
+radius_pluto = np.zeros(np.size(et_vals))
+radius_bary  = np.zeros(np.size(et_vals)) # Distance from the barycenter
+lon          = np.zeros(np.size(et_vals))
+lat          = np.zeros(np.size(et_vals))
+
+for i,et_i in enumerate(et_vals):
 
 # Define a ray from NH, toward the star.
 # Ray starts at NH position and points toward star (*not* boresight). It should be in IAU_PLUTO coordxmas.
 
 # Get vector from Pluto to S/C, in IAU_PLUTO. This will define the *point* (the ray is a point plus a vector)
 
-    (st_plu_sc_plu, lt) = cspice.spkezr('NEW_HORIZONS', et_i, 'IAU_PLUTO', 'LT+S', 'PLUTO')
-    pt_plu_sc_plu = st_plu_sc_plu[0:3]
+    (st_plu_sc_plu_i, lt_i) = cspice.spkezr('NEW_HORIZONS', et_i, 'IAU_PLUTO', 'LT+S', 'PLUTO BARYCENTER') # Need to change this to barycenter
+    pt_plu_sc_plu_i = st_plu_sc_plu_i[0:3]
 
-# Get vector from Pluto to star, in IAU_PLUTO. This will define the *vector* portion of the ray.
-
-    vec_plu_star = cspice.radrec(1., ra_star, dec_star) # Vector from Pluto to star, in J2K
-    mx = cspice.pxform('J2000', 'IAU_PLUTO', et_i)
-    vec_plu_star_plu = cspice.mxvg(mx, vec_plu_star)
+    mx_i = cspice.pxform('J2000', 'IAU_PLUTO', et_i)
+    vec_plu_star_plu_i = cspice.mxvg(mx_i, vec_plu_star)
 
 # Now find the intersection. A ray is *not* a datatype. Instead, we just pass a point and a vector, and that defines the ray.
 # Point = vector from Pluto to NH in Pluto coords.
 # Vector = vector from Pluto star in Pluto coords
 
-    (npts, pt_intersect_plu) = cspice.inrypl(pt_plu_sc_plu, vec_plu_star_plu, plane_plu) # intersect ray and plane. Jup coords.
+    (npts, pt_intersect_plu_i) = cspice.inrypl(pt_plu_sc_plu_i, vec_plu_star_plu_i, plane_plu) # intersect ray and plane. Jup coords.
 
-    (radius, lon, lat) = cspice.reclat(pt_intersect_plu)  # Convert to lon/lat and distance from Pluto
-
-    print cspice.et2utc(et_i, 'C', 1)
-    print "Intercept distance = " + hbt.trunc(radius,1) + " km from Pluto"
-#    print "NH distance = " + hbt.trunc(cspice.vnorm(pt_plu_sc_plu),1) + " km from Pluto"
-    print
-    
-# Call CSPICE_INRYPL to get the intersection between ray and plane.
-
-# Then get the distance from this point, to Pluto (or alternatively, the Pluto system barycenter)
-
-quit
+    (radius_bary[i], lon[i], lat[i]) = cspice.reclat(pt_intersect_plu_i)  # Convert to lon/lat and distance from Pluto
 
 ####################
 # Now do some testing to read in the housekeeping data. Randy says that the same data from the main FITS header should be
@@ -424,33 +438,71 @@ hk.data
 # Now do a plot with differeng binning widths. To look for narrow rings.
 #==============================================================================
 
-binning = [1, 30, 300, 3000, 30000]
+#binning = [1, 30, 300, 3000, 30000]
 
-plt.rcParams['figure.figsize'] = 20,12
+#plt.rcParams['figure.figsize'] = 20,12
+plt.rcParams['figure.figsize'] = 10,6
 
-count_rate_fixed_30000 = hbt.smooth_boxcar(count_rate_fixed, 30000)
-count_rate_fixed_3000 = hbt.smooth_boxcar(count_rate_fixed, 3000)
-count_rate_fixed_300 = hbt.smooth_boxcar(count_rate_fixed, 300)
-count_rate_fixed_30 = hbt.smooth_boxcar(count_rate_fixed, 30)
+host = host_subplot(111, axes_class=AA.Axes) # Set up the host axis
+par = host.twiny()                           # Set up the parasite axis
+plt.subplots_adjust(bottom=0.2)              # Adjusts overall height of the whole plot in y direction (like figure.figsize)
+offset = 50                                  # How far away from the main plot the parasite axis is. Don't know the units.
+new_fixed_axis     = par.get_grid_helper().new_fixed_axis
+par.axis["bottom"] = new_fixed_axis(loc="bottom", axes=par,
+                                    offset=(0,-offset))
+par.axis["bottom"].toggle(all=True)          # Make sure the bottom axis is displayed
+par.axis["top"].set_visible(False)           # Do not display the axis on *top* of the plot.
 
-plt.plot(t, count_rate, ms=0.1, linestyle='none', marker='.', color='orange', 
-         label = 'Alice, Raw, 250/sec [orange]')
-plt.plot(t, count_rate_fixed_30, linewidth=0.1, ms=0.1, color='green', 
-         label='Alice, smoothed 30 bins = ' + repr(30*dt) + ' sec [green]' )
-plt.plot(t, count_rate_fixed_300, linewidth=0.1, ms=0.1, color='blue', 
-         label='Alice, smoothed 300 bins = ' + repr(300*dt) + ' sec [blue]')
-plt.plot(t, count_rate_fixed_3000, linewidth=0.3, ms=0.1, color='red', 
-         label='Alice, smoothed 3000 bins = ' + repr(3000*dt) + ' sec [red]')
-plt.plot(t, count_rate_fixed_30000, linewidth=0.2, ms=0.1, color='yellow', 
-         label='Alice, smoothed 30000 bins = ' + repr(30000 * dt) + ' sec [yellow]' )
-plt.ylim((14,20))
+p1, = host.plot(t, count_rate, ms=0.1, linestyle='none', marker='.', color='orange', 
+         label = 'Alice, Raw, 250/sec = 1.5 m [orange]')
+host.plot(t, count_rate_fixed_30, linewidth=0.1, ms=0.1, color='green', 
+         label='Alice, smoothed 30 bins = ' + repr(30*dt) + ' = 4 sec = 40 m[green]' )
+host.plot(t, count_rate_fixed_300, linewidth=0.1, ms=0.1, color='blue', 
+         label='Alice, smoothed 300 bins = ' + repr(300*dt) + ' sec = 0.4 km [blue]')
+host.plot(t, count_rate_fixed_3000, linewidth=0.3, ms=0.1, color='red', 
+         label='Alice, smoothed 3000 bins = ' + repr(3000*dt) + ' sec = 4 km [red]')
+host.plot(t, count_rate_fixed_30000, linewidth=0.2, ms=0.1, color='yellow', 
+         label='Alice, smoothed 30000 bins = ' + repr(30000 * dt) + 
+         ' sec = 40 km [yellow]' )
+host.set_ylim((14,20))
 plt.xlim(hbt.mm(t))
+par.set_xlim(hbt.mm(radius_bary))
 plt.title(sequence, fontsize=fs)
 plt.ylabel('DN', fontsize=fs)
 plt.xlabel('Seconds since ' + utc_start, fontsize=fs)
+par.set_xlabel('Distance from Pluto barycenter [km]')
+
 plt.legend()
 plt.show()
 
+#
+
+
+
+
+
+
+host.set_xlim(0, 2)
+host.set_ylim(0, 2)
+
+host.set_xlabel("Distance")
+host.set_ylabel("Density")
+par.set_xlabel("Velocity")
+
+p1, = host.plot([0, 1, 2], [0, 1, 2], label="Density")
+
+par.set_ylim(1, 65)
+
+host.axis["left"].label.set_color(p1.get_color())
+par.axis["right"].label.set_color(p3.get_color())
+
+plt.draw()
+plt.show()
+
+
+
+# Put another axis here: et vs. radius_pluto
+ 
 #==============================================================================
 # Do a histogram of the count rate
 #==============================================================================
