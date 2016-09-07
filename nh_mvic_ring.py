47b1e753f629a8ea3bd4404a111c686d7368d6a9
Henry Throop <henry.throop@gmail.com>
2016-09-08 00:25:07 +0530

    New file for MVIC analysis.

---
+++ nh_mvic_ring.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  7 22:55:34 2016
+
+@author: throop
+"""
+
+dir = '/Users/throop/Data/NH_MVIC_Ring/'
+file = 'mvic_d305_sum_mos_v1.fits'
+
+import pdb
+import glob
+import math       # We use this to get pi. Documentation says math is 'always available' 
+                  # but apparently it still must be imported.
+from   subprocess import call
+import warnings
+import pdb
+import os.path
+import os
+import subprocess
+
+import astropy
+from   astropy.io import fits
+from   astropy.table import Table
+import astropy.table   # I need the unique() function here. Why is in in table and not Table??
+import matplotlib.pyplot as plt # pyplot
+from   matplotlib.figure import Figure
+import numpy as np
+import astropy.modeling
+from   scipy.optimize import curve_fit
+#from   pylab import *  # So I can change plot size.
+                       # Pylab defines the 'plot' command
+import cspice
+import skimage
+from   itertools import izip    # To loop over groups in a table -- see astropy tables docs
+from   astropy.wcs import WCS
+from   astropy.vo.client import conesearch # Virtual Observatory, ie star catalogs
+from   astropy import units as u           # Units library
+from   astropy.coordinates import SkyCoord # To define coordinates to use in star search
+#from   photutils import datasets
+from   astropy.stats import sigma_clipped_stats
+from   scipy.stats import mode
+from   scipy.stats import linregress
+from   photutils import daofind
+import wcsaxes
+import time
+from scipy.interpolate import griddata
+
+import imreg_dft as ird
+import re # Regexp
+import pickle # For load/save
+
+# Imports for Tk
+
+import Tkinter
+import ttk
+import tkMessageBox
+from   matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from   matplotlib.figure import Figure
