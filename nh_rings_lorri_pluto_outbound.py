168cae33da9b45fde0301bd18a5355d07b9a84b9
Henry Throop <henry.throop@gmail.com>
2016-12-07 13:37:09 +0530

    New file to look at outbound Pluto LORRI images

---
+++ nh_rings_lorri_pluto_outbound.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 13:28:06 2016
+
+@author: throop
+"""
+
+# Read LORRI outbound Pluto rings mosaics and calculate I/F limit from them.
+# This is not a rigorous reduction -- just want to get an answer so I can write an MT
+# about using LORRI vs. MVIC for unresolved KEM observations. 
+# HBT 7-Dec-2016
+
+dir = 
