6723f2635d5854a2493d9a79842a2bd5c01b46e3
Henry Throop <henry.throop@gmail.com>
2017-04-25 13:56:17 +0530

    Simple user interface to interactively navigate all LORRI rings images. Works on most images.

--- nh_jring_navigate_images.py
+++ nh_jring_navigate_images.py
@@ -7,12 +7,10 @@ Created on Wed Apr 19 16:15:18 2017
 """
 
 """
-Library routine to navigate an image based on stars.
-  o Use DAOPhot to locate stars in the image
-  o Use a star catalog to look up stars that should be in the image
-  o Calculate the offset between these.
-  o Return the result, in terms of a revised WCS for the frame, and a pixel offset.
-  o Does not rewrite the FITS header.
+User-level program to interactively navigate all LORRI rings images.
+Allows user to move around within image list, view images, navigate automatically, write
+new WCS coords out to .fits file, etc.
+This is essentially a user interface to hbt.navigate_image_stellar().
 """
 
 # General python imports
@@ -45,11 +43,8 @@ from   astropy.wcs import WCS
 from   astropy.vo.client import conesearch # Virtual Observatory, ie star catalogs
 from   astropy import units as u           # Units library
 from   astropy.coordinates import SkyCoord # To define coordinates to use in star search
-from   scipy.stats import mode
-from   scipy.stats import linregress
-import wcsaxes
 import time
-from   scipy.interpolate import griddata
+import sys
 #import cv2
 
 import re # Regexp
@@ -65,9 +60,9 @@ from   time import gmtime, strftime
 
 # HBT imports
 
-import hbt
+import hbt 
 
-#def nh_jring_navigate_images():
+#nh_jring_navigate_images():
 
 """
 Navigate all the NH Jring images
@@ -80,73 +75,137 @@ Navigate all the NH Jring images
 ##    file_in = dir + 'lor_0034676528_0x630_sci_1_opnav.fit' # Post-navigation
 ##    file_in = dir + 'lor_0034676528_0x630_sci_1_opnav_opnav.fit' # Post-navigation
 
-
 dir =  '/Users/throop/Dropbox/Data/NH_Jring/data/jupiter/level2/lor/all/' 
 
 files = glob.glob(dir + '*[123456].fit')  # Exclude any '*_opnav.fit'
 
 plt.set_cmap('Greys_r')            
 hbt.figsize((10,10))
-do_plot = True
+do_plot           = True
+DO_SKIP_NAVIGATED = False
+DO_INTERACTIVE    = True
+method_opnav      = 'fft'
+is_success        = False
 
-for i,file in enumerate(files):
-    
-    file_out = file.replace('.fit', '_opnav.fit')
-    
-    im = hbt.read_lorri(file) # Read the image, and process it a bit I think
-    
-    hdulist = fits.open(file) 
-    header = hdulist['PRIMARY'].header
-    mode = header['SFORMAT']     
-    hdulist.close()           
-    
-# Want to only do this if it is LORRI 1x1, not 4x4.
-# Also, skip if this file has already been done?
+i = 0   # i must be the current image number
+        # ii is index within the list
+        # k is the keyboard string
 
-    if ((mode == '1X1') and (not(os.path.isfile(file_out)))):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")    
-            w_orig  = WCS(file)           # Look up the WCS coordinates for this frame
-            w       = WCS(file)           # Get a copy of it, which we'll change
+# Now start a keyboard loop and run with input from the user
+
+while True:
+    
+    file_short = files[i].split('/')[-1]
+    
+    if DO_INTERACTIVE:
+        k = input("File {}-{} ({} = {}): ".format(0, np.size(files)-1, i, file_short))
+    else:
+        k = repr(list_batch[ii])         # Load the next element, as a string
+        if (ii == np.size(list_batch)-1):  # If we've hit the last element
+            DO_INTERACTIVE = True    # And go back to interactive mode
+
+    if (k in ['x', 'q']):            # QUIT
+        sys.exit(0)
+
+    if ('-' in k):
+       (i1, i2) = np.array(k.split('-')).astype('int')
+       list_batch = hbt.frange(int(i1), int(i2)).astype('int')
+       ii = 0                        # Current element number, within our list
+       k = repr(list_batch[ii])      # Extract one element from it
+       print ("Running from {} to {}...".format(i1, i2))
+       DO_INTERACTIVE = False
+       
+    if (k == 'l'):                  # List all files
+        for ii,file in enumerate(files):
+            print("{}. {}".format(ii, file.split('/')[-1]))
+    
+    if (k == '?'):                  # Get help
+        print(" <#> = navigate, <#-#> = navigate range, l = list, n = next, x = exit")
+    
+    if (k == 'n') :                 # Next
+        k = repr(i)
+
+    if (k == ''):                   # Next
+        k = repr(i)
+    
+    if hbt.is_number(k):            # If a number was entered () 
+        print("I = {}".format(i))
+        i = int(k)
+        file = files[i]
+        file_short = file.split('/')[-1]
+        file_out   = file.replace('.fit', '_opnav.fit')
         
-        crval_orig = w_orig.wcs.crval
+        im = hbt.read_lorri(file) # Read the image, and process it a bit I think
+        print("Reading {}/{}: {}".format(int(k), np.size(files), file_short))
+        hdulist = fits.open(file) 
+        header  = hdulist['PRIMARY'].header
+        mode    = header['SFORMAT']
+        exptime = header['EXPTIME']
+        hdulist.close()           
+        
+        is_navigated = os.path.isfile(file_out)
+        
+        if (mode == '4X4'):
+            print("{}/{}: Skipping due to 4x4".format(i, np.size(files)))
+            
+        elif (is_navigated and DO_SKIP_NAVIGATED):
+            print ("{}/{}: Skipping since already navigated".format(i, np.size(files)))
     
+        else:
+            
+            with warnings.catch_warnings():   # Block the warnings
+                warnings.simplefilter("ignore")    
+                w_orig  = WCS(file)           # Look up the WCS coordinates for this frame
+                w       = WCS(file)           # Get a copy of it, which we'll change
+            
+            crval_orig = w_orig.wcs.crval
+        
     # Do the navigation call
-    
-        method_opnav = 'fft'
-        (w, (dy_pix, dx_pix)) = hbt.navigate_image_stellar(im, w, method = method_opnav,
-                                 title = file.split('/')[-1], do_plot=do_plot)
-    
-        crval = w.wcs.crval
-    
-        print("Center was at RA {}, Dec {}".format(crval_orig[0], crval_orig[1]))
-        print("Center is now at RA {}, Dec {}".format(crval[0], crval[1]))
-        print("Determined OpNav offset: dx = {} pix, dy = {} pix".format(dx_pix, dy_pix))
-    
+                    
+            (w, (dy_pix, dx_pix)) = hbt.navigate_image_stellar(im, w, method = method_opnav,
+                                     title = file.split('/')[-1], do_plot=do_plot)
+        
+            crval = w.wcs.crval
+        
+            print("Center was at RA {}, Dec {}".format(crval_orig[0], crval_orig[1]))
+            print("Center is now at RA {}, Dec {}".format(crval[0], crval[1]))
+            print("Determined OpNav offset: dx = {} pix, dy = {} pix".format(dx_pix, dy_pix))
+        
     # Read in the existing FITS file to memory, so we can modify it and write it back out
     # Q: Is it just necessary to update these fields, or do I need to do more?
     
     # A: Looks like just updating CRVAL1/2 in the text header itself should be enough.
     #    See http://www.stsci.edu/hst/HST_overview/documents/multidrizzle/ch44.html
     
-        header_wcs = w.to_header()   # NB: w.to_header, w.to_header(), and w.wcs.to_header() all do different things!
-        
-        hdulist = fits.open(file)
-        header  = hdulist[0].header
-        header['CRVAL1'] = crval[0] # RA, deg
-        header['CRVAL2'] = crval[1] # Dec, deg
-        header['OPNAVDX'] = (dx_pix, '[pix] Offset determined from stellar navigation, X')
-        header['OPNAVDY'] = (dy_pix, '[pix] Offset determined from stellar navigation, Y')
-        header['OPNAVMTH'] = (method_opnav, 'Method for OpNav stellar navigation')
-        header['comment'] = 'OpNav CRVAL via navigate_image_stellar, ' + \
-                             strftime("%Y-%m-%d %H:%M:%S UTC", gmtime())
-    
-    # Write out a revised FITS file
-    
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")            # Ignore warning about a long comment.
-            hdulist.writeto(file_out, overwrite=True)  # Write to a new file (open, write, and close, in one command)
+            header_wcs = w.to_header()   # NB: w.to_header, w.to_header(), and w.wcs.to_header() all do different things!
             
-        hdulist.close()                            # Close the original file
+            hdulist = fits.open(file)
+            header  = hdulist[0].header
+            header['CRVAL1'] = crval[0] # RA, deg
+            header['CRVAL2'] = crval[1] # Dec, deg
+            header['OPNAVDX'] = (dx_pix, '[pix] Offset determined from stellar navigation, X')
+            header['OPNAVDY'] = (dy_pix, '[pix] Offset determined from stellar navigation, Y')
+            header['OPNAVMTH'] = (method_opnav, 'Method for OpNav stellar navigation')
+            header['comment'] = 'OpNav CRVAL via navigate_image_stellar, ' + \
+                                 strftime("%Y-%m-%d %H:%M:%S UTC", gmtime())
         
-        print("Wrote file {}/{}: ".format(i,len(files), file_out))
+    # Write out a revised FITS file
+        
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")            # Ignore warning about a long comment.
+                hdulist.writeto(file_out, overwrite=True)  # Write to a new file (open, write, and close, in one command)
+                
+            hdulist.close()                            # Close the original file
+            
+            print("Wrote file {}/{}: {}".format(i,len(files), file_out))
+            
+            is_success = True
+            
+    else:
+        print("Error!")
+        is_success = False
+        
+    if (DO_INTERACTIVE and is_success):
+        i += 1        # Go to next image in master list
+    else:
+        ii += 1       # Go to next image in the sublist
