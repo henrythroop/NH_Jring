d5f914228f9f51882bac77378b7d7ba9360d51d4
Henry Throop <henry.throop@gmail.com>
2017-08-11 16:21:44 +0530

    Added several different methods for radial profile extraction (e.g., to take radial profile of entire ring, but exclude the ansa).

--- nh_jring_extract_profile_from_unwrapped.py
+++ nh_jring_extract_profile_from_unwrapped.py
@@ -56,20 +56,46 @@ def nh_jring_extract_profile_from_unwrapped(im_unwrapped, bins_radius, bins_azim
 
 #  We use a subset of the azimuth angles.
 #    e.g., if range=0.1, then extract the central 10% of the azimuth angles
+#    e.g., if range=(0.9, -0.4), then extract the central 90% of azimuth angles, but then *exclude* 
+#         the central 40% of angles          
     
-        bin_0 = int(hbt.sizex(bins_azimuth) * (0.5 - (0.5 * range_profile)))  
-        bin_1 = int(hbt.sizex(bins_azimuth) * (0.5 + (0.5 * range_profile)))
+        # In case of a single range -- e.g., 0.1
+        
+        if isinstance(range_profile, float) or isinstance(range_profile, int):
+            bin_0 = int(hbt.sizex(bins_azimuth) * (0.5 - (0.5 * range_profile)))  
+            bin_1 = int(hbt.sizex(bins_azimuth) * (0.5 + (0.5 * range_profile)))
+    
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=RuntimeWarning)    
+                profile_radius  = np.nanmean(im2[:, bin_0:bin_1],1)
+        
+        # In case of a double range -- e.g., (0.9, -0.3)
+        
+        elif isinstance(range_profile, tuple):
+            bin_0 = int(hbt.sizex(bins_azimuth) * (0.5 - (0.5 * range_profile[0])))  
+            bin_1 = int(hbt.sizex(bins_azimuth) * (0.5 - (0.5 * np.abs(range_profile[1])))) 
+            bin_2 = int(hbt.sizex(bins_azimuth) * (0.5 + (0.5 * np.abs(range_profile[1])))) 
+            bin_3 = int(hbt.sizex(bins_azimuth) * (0.5 + (0.5 * range_profile[0])))
 
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=RuntimeWarning)    
-            profile_radius  = np.nanmean(im2[:, bin_0:bin_1],1)
+            print("Extracting from bins {}:{}, {}:{}".format(bin_0, bin_1, bin_2, bin_3))
+            
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=RuntimeWarning)    
+                profile_radius  = (np.nanmean(im2[:, bin_0:bin_1],1) +
+                                   np.nanmean(im2[:, bin_2:bin_3],1)) / 2
+
+        else:
+            
+            raise(ValueError("Cannot parse range! range_profile = {}".format(range_profile)))
+          
         return(profile_radius)
         
 # Extract the azimuthal profile, if requested
    
     if (type_profile.upper()[0] == 'AZIMUTHAL'[0]):
    
-      if (isinstance(range_profile, float)):   # If passed a single radial range value, like 0.5, then use as fraction
+      if (isinstance(range_profile, float) or isinstance(range_profile, int)):   
+                                        # If passed a single radial range value, like 0.5, then use as fraction
           bin_0 = int(hbt.sizex(bins_radius) * (0.5 - (0.5 * range_profile)))
           bin_1 = int(hbt.sizex(bins_radius) * (0.5 + (0.5 * range_profile)))
 
