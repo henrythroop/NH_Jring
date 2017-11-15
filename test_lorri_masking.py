de3ba4020fda000f8aca7b4481a80c0967c99a3c
Henry Throop <henry.throop@gmail.com>
2017-11-15 16:57:59 +0530

    Doing some testing for effectiveness of different masks on the LORRI 7/32-35 sequence. Includes new file.

--- test_lorri_masking.py
+++ test_lorri_masking.py
@@ -41,24 +41,28 @@ from   scipy.stats import linregress
 #import wcsaxes
 import time
 from   scipy.interpolate import griddata
+from astropy.stats import sigma_clip
 
 import re # Regexp
 import pickle # For load/save
+
+from skimage.io import imread, imsave
 
 # HBT imports
 
 import hbt
 
+plt.set_cmap('Greys_r')
+
 dir_images = '/Users/throop/data/NH_Jring/data/jupiter/level2/lor/all'
 
-dir_out = '/Users/throop/data/NH_Jring/out/'
-
-file = 'lor_0034612923_0x630_sci_1.fit'
+dir_jring = '/Users/throop/data/NH_Jring/'
+dir_out = dir_jring + 'out/'
 
 filename_save = 'nh_jring_read_params_571.pkl' # Filename to save parameters in
 
-index_group_default = 8 # Jupiter ring phase curve
-index_image_default = 20 # Image number within the group
+index_group = 7  # Jupiter ring phase curve
+index_image = 32 # Image number within the group
         
 # Load pickle file
 
@@ -73,9 +77,6 @@ lun = open(dir_out + filename_save, 'rb')
 t = pickle.load(lun)
 lun.close()
         
-index_group = index_group_default
-index_image  = index_image_default
-
 # Extract the one group we are looking at 
 # NB: This creates an entirely new table -- *not* a view into the original table. 
 # If we modify values here, we need to explicitly write the changes back to the original.
@@ -86,32 +87,55 @@ groupmask = t['Desc'] == groups[index_group]
 t_group = t[groupmask]
 
 num_images_group = np.size(t_group)
+
+file = t_group[index_image]['Filename']
         
 # Read the image
 
-hdulist = fits.open(dir_images + '/' + file)
+hdulist = fits.open(file)
 data = hdulist['PRIMARY'].data
-stretch = astropy.visualization.PercentileInterval(10)  # PI(90) scales array to 5th .. 95th %ile. 
 
-# Create the backplane
+# Clip the image, removing extrema
 
-planes = hbt.create_backplane(dir_images + '/' + file)
+#data_clip = sigma_clip(data, sigma=3, iters=10)
 
-am = planes['Ang_Metis']
+stretch = astropy.visualization.PercentileInterval(90)  # PI(90) scales array to 5th .. 95th %ile. 
 
-dx_total =  ( t_group['dx_opnav'][index_image] )
-dy_total =  ( t_group['dy_opnav'][index_image])
+styles = ['', 'aggressive', 'nominal', 'ring_broad_only', 'ring_narrow_only']
 
-(dx_total, dy_total) =(52,70)
-  
-am_roll = np.roll(np.roll(am, -dy_total, axis=0), -dx_total, axis=1)
+for style in styles:
+    if (style):
+        file_mask = dir_jring + '/masks/mask_7_32-35_{}.png'.format(style)
+        mask = (imread(file_mask)[:,:,0])   # Flatten to 2D
+        mask = (mask > 0)
 
-# Plot it
+    else:
+        mask = np.ones(np.shape(data))
+        mask = (mask > 0)
 
-data_stretch = stretch(hbt.remove_sfit(data,degree=5))
+    # Remove the sfit from the masked image
+    
+    out = hbt.remove_sfit(data, mask = mask)
+    
+    # Calculate the min and max values. But do this of the data, where it is masked True.
+    # And, exclude outliers (like CRs) when we do this.
+    
+    out_masked = out[mask]
+    out_masked_clipped = sigma_clip(out_masked, sigma_lower=1000, sigma_upper=3, iters=10)
+    
+    mm = hbt.mm(out_masked_clipped)
 
-plt.imshow(data_stretch)
-plt.show()
+    stretch_mm = astropy.visualization.ManualInterval(vmin=mm[0], vmax=mm[1])
 
-plt.imshow( data_stretch * (am_roll > 0.0001))
-plt.show()
+    # Plot the image, using these properly calculated robust min and maxes, in the masked area only
+    
+    plt.imshow(stretch_mm(out*mask))
+        
+    plt.title(style)
+    plt.show()
+
+# Now I need to take a radial profile of each of these...
+    
+
+
+# OK. We want to scale it to the min and max *within the masked region*.
