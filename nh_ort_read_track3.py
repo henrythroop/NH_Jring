b2f1df6c823fa4e0882575b96b35d55d5e061485
Henry Throop <henry.throop@gmail.com>
2018-02-13 20:41:04 +0530

    New file to read output rom Hamilton / Kaufmann integrations.

---
+++ nh_ort_read_track3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 15:38:48 2018
+
+@author: throop
+"""
+
+class track3_orbits:
+    
+    def __init__(self):
+        
+        
+        return
+
+if __name__ == '__main__':
+    
+    file = '/Users/throop/data/ORT1/kaufmann/deliveries/ort1-0001/1000010/speed1/beta_1.2e-03/subset00/model.array2'
+    
+    track = track3_orbits()
+     
