b60f9d0643ccf45a61e5a71216f7a6c0fffc2c95
Henry Throop <henry.throop@gmail.com>
2018-02-14 16:32:15 +0530

    Development.

--- nh_ort_read_track3.py
+++ nh_ort_read_track3.py
@@ -58,7 +58,7 @@ from   matplotlib.figure import Figure
 
 import hbt
 
-class ort_track3:  # Q: Is this required to be same name as the file?
+class nh_ort_read_track3:  # Q: Is this required to be same name as the file?
 
 # =============================================================================
 # Initialize the method.
@@ -87,16 +87,27 @@ class ort_track3:  # Q: Is this required to be same name as the file?
 
         dir = name
         
+        # Construct the filename for the 'header', which is a short text file. It is basically the same
+        # as the entire directory tree, but with '/' → '_'.
+        # There is one difference -- probably a bug -- in that 'speed' term is off by one, as per DK email. OBOB.
+        
         file_header =(parts[0].replace('-', '_') + '_' + parts[1] + '_' + 
                       parts[2].replace('1', '0').replace('2', '1') + '_' +
                       parts[3] + '_' + parts[4] +
                       '.header')
 
+        # Construct the filename for the data array itself. It is trivial.
+        # As per the wiki, the data and header files should be identically named. They are not!
+        # https://www.spaceops.swri.org/nh/wiki/index.php/KBO/Hazards/Pipeline/Integrations-to-Density
+        
         file_data = f'model.array{binfmt}'
         
         file_header_full = os.path.join(dir_base, dir, file_header)
         file_data_full   = os.path.join(dir_base, dir, file_data)
 
+        self.file_header_full = file_header_full
+        self.file_data_full   = file_data_full
+        
         # Read the datafiles. Using code fragment from DK email 13-Feb-2018
 
         # Read header file
@@ -105,6 +116,9 @@ class ort_track3:  # Q: Is this required to be same name as the file?
         text = f.read()
         f.close()
         lines = text.split('\n')
+        
+        self.header = lines
+        
         nx = int(lines[0].split()[0])
         ny = int(lines[1].split()[0])
         nz = int(lines[2].split()[0])
@@ -135,15 +149,54 @@ class ort_track3:  # Q: Is this required to be same name as the file?
                 (ix,iy,iz) = struct.unpack('hhh', data[start:start+6])
                 (density[iz-1,iy-1,ix-1],) = struct.unpack('f', data[start+6:start+10])
     
+    
         self.density = density
         self.km_per_cell_x = km_per_cell_x
         self.km_per_cell_y = km_per_cell_y
         self.km_per_cell_z = km_per_cell_z
         
+        self.body_id   = self.get_header_val('body_id')
+        self.duration  = self.get_header_val('duration')*u.year
+        self.time_step = self.get_header_val('time_step')*u.s
+        self.beta      = self.get_header_val('beta')
+        self.weight    = self.get_header_val('weight')   # Statistical weight of this solution
+        self.speed     = self.get_header_val('speed')    # Should be 1 or 2 as per wiki. But is really 0 or 1. 
+        
         # We should read the full header in to the class as well. But this will require more parsing.
         
         return
 
+# =============================================================================
+# Search through the header, and find the value corresponding to a given keyword
+# =============================================================================
+
+    def get_header_val(self, keyword):
+        
+        """ 
+        Get a header value from a list of header lines.
+        
+        As per https://www.spaceops.swri.org/nh/wiki/index.php/KBO/Hazards/Pipeline/Integrations-to-Density,
+        the header is supposed to be a fixed format, with one specific value per line. However, I am building
+        this function more robustly, in case the specs change in the future.
+        
+        Parameters
+        -----
+        
+        keyword:
+            A string that matches the keyword for a specified entry in the header. `keyword` should be chosen so that
+            it will not match multiple lines in the header.
+        
+        """
+       
+                                                 
+        out = ''
+        for line in self.header:                    # Loop over every line
+            if keyword in line:
+                out = line.split('#')[0].strip()
+                if hbt.is_number(out):                  # Convert from string to float, if possible
+                    out = float(out)
+        return out
+       
 # =============================================================================
 # Plot the distributions
 # =============================================================================
@@ -169,6 +222,11 @@ class ort_track3:  # Q: Is this required to be same name as the file?
                 plt.colorbar()
             plt.title(self.name)
         
+        plt.subplot(2,2,4)
+        plt.imshow((np.sum(self.density, axis=0)), extent=extent)
+        plt.xlim((-10000,10000))
+        plt.ylim((-10000,10000))
+        
         plt.show()
         
         return
@@ -182,10 +240,11 @@ if __name__ == '__main__':
     dir_base='/Users/throop/data/ORT1/kaufmann/deliveries'
     
     runs_full = glob.glob(os.path.join(dir_base, '*', '*', '*', '*', '*'))
-
+    run = runs_full[0].replace(dir_base, '')[1:]
+    
     for run_full in runs_full:
-        run = run_full.replace(dir_base, '')[1:]
-        ring = ort_track3(run)
+        run = run_full.replace(dir_base, '')[1:]  # Remove the base pathname from this, and initial '/'
+        ring = nh_ort_read_track3(run)
         ring.plot()
     
      
