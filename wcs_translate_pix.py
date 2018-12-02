730ace66f07d8983377aa1cd59c20ae7b9ecca6d
Henry Throop <henry.throop@gmail.com>
2018-12-01 23:06:10 -0500

    Fixed bugs in wcs_translate()

--- wcs_translate_pix.py
+++ wcs_translate_pix.py
@@ -97,9 +97,11 @@ def wcs_zoom(wcs, zoom, shape_orig):
     # doing weird manipulations to make up for my errors over there. That is basically OK though -- 
     # as long as I am consistent and get the same answers both ways in the end.
 
-
-    crpix_new = np.array( [(crpix[0]+0.5)/shape_orig[0] * (zoom * shape_orig[0]) + 1 - (3/2.) * zoom,
-                           (crpix[1]+0.5)/shape_orig[1] * (zoom * shape_orig[1]) + 1 - (3/2.) * zoom] )
+    crpix_new = np.array( [crpix[0] * zoom,
+                          crpix[1] * zoom] )
+    
+    # crpix_new = np.array( [(crpix[0]+0.5)/shape_orig[0] * (zoom * shape_orig[0]) + 1 - (3/2.) * zoom,
+                           # (crpix[1]+0.5)/shape_orig[1] * (zoom * shape_orig[1]) + 1 - (3/2.) * zoom] )
     
     print(f'After: crpix={crpix_new}')
     
@@ -124,54 +126,52 @@ def wcs_zoom(wcs, zoom, shape_orig):
     
 if (__name__ == '__main__'):
     
-    file = '/Users/throop/Data/MU69_Approach/porter/KALR_MU69_OpNav_L4_2018301/lor_0403016638_0x633_pwcs2.fits'
+    file = '/Users/throop/Data/MU69_Approach/throop/backplaned/' + \
+           'KALR_MU69_OpNav_L4_2018335/lor_0405944938_0x633_pwcs2_backplaned.fits'
     hdulist = fits.open(file)
     arr = hdulist['PRIMARY'].data
+    header = hdulist['PRIMARY'].header
+    et =  header['SPCSCET']
     hdulist.close()
     wcs = WCS(file)
-
     hbt.figsize((12,12))    
-    plot_img_wcs(arr,wcs, title='Original')
     
-    dx_pix = 12
-    dy_pix = 27
+    plot_img_wcs(arr,wcs, title='Original',
+                 name_observer='New Horizons', name_target='MU69', et=et)
     
-    print(f'Center = {wcs.wcs.crval}')
-    wcs_translate_pix(wcs, dx_pix, dy_pix)
-    print(f'Center = {wcs.wcs.crval}')
+    # dx_pix = 12
+    # dy_pix = 27
+    
+    # print(f'Center = {wcs.wcs.crval}')
+    # wcs_translate_pix(wcs, dx_pix, dy_pix)
+    # print(f'Center = {wcs.wcs.crval}')
 
-    print()
+    # print()
     
-    wcs_translate_pix(wcs, -dx_pix, -dy_pix)
-    print(f'Center = {wcs.wcs.crval}')
+    # wcs_translate_pix(wcs, -dx_pix, -dy_pix)
+    # print(f'Center = {wcs.wcs.crval}')
     
-    # Now, read in an image, pad it, translate it, and see if I can keep the WCS properly synced with the image.
+    # # Now, read in an image, pad it, translate it, and see if I can keep the WCS properly synced with the image.
 
-    wcs = WCS(file)
+    # wcs = WCS(file)
     
-    plot_img_wcs(arr,wcs, title='Original')
-
-    dxy_pad = 10
-    arr_pad = np.pad(arr, ((dxy_pad, dxy_pad),(dxy_pad, dxy_pad)), 'constant')
-    wcs_pad = wcs.deepcopy()
-    wcs_translate_pix(wcs_pad, dxy_pad, dxy_pad)
-    plot_img_wcs(arr_pad, wcs_pad, title=f'Padded by {dxy_pad}')
-    
-    arr_pad_trans = np.roll(np.roll(arr_pad, dx_pix, axis=1), dy_pix, axis=0)
-    wcs_pad_trans = wcs_pad.deepcopy()
-    wcs_translate_pix(wcs_pad_trans, dx_pix, dy_pix)
-    plot_img_wcs(arr_pad_trans, wcs_pad_trans, title=f'Padded by {dxy_pad}')
+    # dxy_pad = 10
+    # arr_pad = np.pad(arr, ((dxy_pad, dxy_pad),(dxy_pad, dxy_pad)), 'constant')
+    # wcs_pad = wcs.deepcopy()
+    # wcs_translate_pix(wcs_pad, dxy_pad, dxy_pad)
+    # plot_img_wcs(arr_pad, wcs_pad, title=f'Padded by {dxy_pad}', 
+    #              name_observer='New Horizons', name_target='MU69', et=et)
 
     # Now test zooming. Start from scratch
     
-    file = '/Users/throop/Data/MU69_Approach/porter/KALR_MU69_OpNav_L4_2018301/lor_0403016638_0x633_pwcs2.fits'
-    hdulist = fits.open(file)
-    arr_orig = hdulist['PRIMARY'].data
-    hdulist.close()
-    wcs = WCS(file)
+    # hdulist = fits.open(file)
+    # arr_orig = hdulist['PRIMARY'].data
+    # hdulist.close()
+    # wcs = WCS(file)
     
     arr = arr_orig.copy()
-    plot_img_wcs(arr, wcs, title='original')
+    plot_img_wcs(arr, wcs, title='original',
+                 name_observer='New Horizons', name_target='MU69', et=et)
 
     # Zoom
 
@@ -179,7 +179,8 @@ if (__name__ == '__main__'):
     wcs_zoomed = wcs.deepcopy()
     wcs_zoom(wcs_zoomed, zoom, np.shape(arr_pad))
     arr_zoom = scipy.ndimage.zoom(arr, zoom)
-    plot_img_wcs(arr_zoom, wcs_zoomed, title=f'Zoomed by {zoom}')
+    plot_img_wcs(arr_zoom, wcs_zoomed, title=f'Zoomed by {zoom}',name_observer='New Horizons', 
+                 name_target='MU69', et=et, width=100)
 
     # Translate, then zoom
     zoom = 1
@@ -189,17 +190,19 @@ if (__name__ == '__main__'):
     arr_trans = np.roll(np.roll(arr, dx_pix, axis=1), dy_pix, axis=0)
     wcs_zoom(wcs_zoomed, zoom, np.shape(arr_pad_trans))
     wcs_translate_pix(wcs_zoomed, dx_pix, dy_pix)
-    plot_img_wcs(arr_trans, wcs_zoomed, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}')
+    plot_img_wcs(arr_trans, wcs_zoomed, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}',
+                 name_target='MU69', et=et, width=100)
 
     # Translate, then zoom.
 
-    zoom = 2
-    dx_pix = 30
-    dy_pix = 60
+    zoom = 6
+    dx_pix = -5
+    dy_pix = 5
     wcs2 = wcs.deepcopy()
     arr2 = arr.copy()
     
-    plot_img_wcs(arr2, wcs2, title='Original')
+    plot_img_wcs(arr_trans, wcs_zoomed, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}',
+             name_observer='New Horizons', name_target='MU69', et=et, width=72)
     
     # Roll and zoom the image
     
@@ -210,9 +213,12 @@ if (__name__ == '__main__'):
     
     wcs_translate_pix(wcs2, dx_pix, dy_pix)
     wcs_zoom(wcs2, zoom, np.shape(arr))
-    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}')
+    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}',
+             name_observer='New Horizons', name_target='MU69', et=et, width=800)
+                 
     
     # Translate
+    # This is a bunch of consecutive translations and zooms. It's doing pretty well.
     
     zoom = 0.5
 
@@ -220,10 +226,12 @@ if (__name__ == '__main__'):
     arr2 = scipy.ndimage.zoom(arr2,zoom)
     wcs_translate_pix(wcs2, dx_pix, dy_pix)
     wcs_zoom(wcs2, zoom, np.shape(arr2))
-    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}')
+    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}',
+                 name_observer='New Horizons', name_target='MU69', et=et)
 
     dx_pix = -10
     dy_pix = -30        
     arr2 = np.roll(np.roll(arr2, dx_pix, axis=1), dy_pix, axis=0)
     wcs_translate_pix(wcs2, dx_pix, dy_pix)
-    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}')
+    plot_img_wcs(arr2, wcs2, title=f'Translate by {dx_pix}, {dy_pix}, zoomed by {zoom}',
+                 name_observer='New Horizons', name_target='MU69', et=et, width=100)
