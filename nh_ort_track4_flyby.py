c0060a7c3bfdab56012b48803b7baf4f672bf6ec
Henry Throop <henry.throop@gmail.com>
2018-12-09 16:41:33 -0500

    Fixed trajectory output for MRS. It was independent of gridsize, so results were incorrect  for 500 km grids. Now adds gridsize to filename for clarity.

--- nh_ort_track4_flyby.py
+++ nh_ort_track4_flyby.py
@@ -433,7 +433,7 @@ def make_table_grid_positions():
 #%%%
      
     name_trajectory = 'alternate'  # ← Set this to 'prime' or 'alternate'
-    name_trajectory = 'prime'  # ← Set this to 'prime' or 'alternate'
+    # name_trajectory = 'prime'  # ← Set this to 'prime' or 'alternate'
    
     hbt.unload_kernels_all() 
    
@@ -450,10 +450,14 @@ def make_table_grid_positions():
     file_in = '/Users/throop/Data/ORT5/kaufmann/deliveries/chr3-tunacan10k/chr3-0003' + \
               '/y3.0/beta1.0e+00/subset00/model.array2'
     
-    file_in = '/Users/throop/Data/ORT5/throop/deliveries/tuna9k/ort5_None_y3.0_q3.5_pv0.70_rho1.00.dust.pkl'
+    # file_in = '/Users/throop/Data/ORT5/throop/deliveries/tuna9k/ort5_None_y3.0_q3.5_pv0.70_rho1.00.dust.pkl' #250km
+    # file_in = '/Users/throop/Data/ORT5/throop/deliveries/dph-sunflower10k/ort5_None_y2.2_q2.5_pv0.05_rho0.46.dust.pkl' #500km
+    file_in = '/Users/throop/Data/ORT5/throop/deliveries/dph-tunacan3.5kinc55/ort5_None_y2.2_q2.5_pv0.05_rho0.46.dust.pkl' #500km
     
     grid = nh_ort_track4_grid(file_in)    # Load the grid from disk. Uses gzip, so it is quite slow (10 sec/file)
 
+    resolution_km = int(grid.resolution_km[0])
+    
     utc_ca = '2019 1 Jan 05:33:00'
     dt_before = 1*u.hour
     dt_after  = 1*u.hour
@@ -542,7 +546,7 @@ def make_table_grid_positions():
   
     # Save the table to a file
     
-    file_out = f'positions_trajectory_{name_trajectory}.txt'
+    file_out = f'positions_trajectory_{name_trajectory}_res{resolution_km}.txt'
     path_out = os.path.join('/Users/throop/Data/ORT5', file_out)
     
     t.write(path_out, format = 'ascii.csv', overwrite=True)
@@ -560,8 +564,8 @@ if (__name__ == '__main__'):
     
     # NB: It would make sense to parallelize this loop below. That way I could run a bunch of these in parallel.
     
-    name_trajectory = 'prime'
-    # name_trajectory = 'alternate'  # 'prime' or 'alternate'. For ORT5, use 'prime' on 3.5k, and 'alternate' on 10k. 
+    # name_trajectory = 'prime'
+    name_trajectory = 'alternate'  # 'prime' or 'alternate'. For ORT5, use 'prime' on 3.5k, and 'alternate' on 10k. 
 
     
     # dir_in  = '/Users/throop/data/ORT4/throop/ort4_bc3_10cbr2_dph/'
@@ -572,6 +576,7 @@ if (__name__ == '__main__'):
 
     # dir_in = '/Users/throop/data/ORT5/throop/deliveries/sun10k-DPH/'
     dir_in = '/Users/throop/data/ORT5/throop/deliveries/dph-tunacan3.5kinc55/'
+    # dir_in = '/Users/throop/data/ORT5/throop/deliveries/dph-sunflower10k/'
 
     # dir_in = '/Users/throop/data/ORT5/throop/deliveries/sun10k_a'
     # dir_in = '/Users/throop/data/ORT5/throop/deliveries/sun10k_b'
