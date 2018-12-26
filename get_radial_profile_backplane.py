d7721e43e2700463c7877d740c5b855b658d3fb8
Henry Throop <henry.throop@gmail.com>
2018-12-26 01:02:14 -0500

    Add quadrant asymmetry radial profiles.

--- get_radial_profile_backplane.py
+++ get_radial_profile_backplane.py
@@ -76,6 +76,82 @@ def get_radial_profile_backplane(im, radius_plane, method='median', num_pts = 10
     
     return (radius_1d, profile_1d)
 
+
+def get_radial_profile_backplane_quadrant(im, radius_plane, longitude_plane, method='median', num_pts = 100, do_std=False):
+
+    """
+    Extract a radial profile from an image. 
+    
+    Uses a backplane passed in.
+    
+    Returns four values, corresponding to the four quadrants.
+    
+    Parameters
+    -----
+    
+    im:
+        Array of data values (e.g., the image).
+        
+    radius_plane:
+        2D array, which is the backplane. Typically this is planes['Radius_eq'].
+    
+    num_pts:
+        Scalar. Number of points to use in the output array.  Output radius is evenly spaced
+        from 0 .. max(radius_plane).
+        
+    Optional parameters
+    -----    
+    
+    method: 
+        String. 'mean' or 'median'.
+        
+    do_stdev: 
+        Boolean. If set, compute the standard deviation, and return in the tuple
+        
+    """
+    
+    radius_1d = hbt.frange(0, int(np.amax(radius_plane)), num_pts)
+    
+    profile_1d    = np.zeros((4, len(radius_1d)))
+    
+    std_1d        = profile_1d.copy()
+    
+    quadrant_plane = np.ceil((np.pi + longitude_plane) * 4 / (np.pi * 2)).astype(int) - 1   # Range = 0 .. 3
+    
+    for i in range(len(radius_1d)-2):
+        
+        for j in range(4):    
+
+        # Identify the pixels which are at the right distance
+        
+            is_good = np.logical_and(np.logical_and(radius_plane >= radius_1d[i],
+                                     radius_plane <= radius_1d[i+1],),
+                                     quadrant_plane == j)
+        
+            print(f'Summing {np.sum(is_good)} cells')
+            
+            if (method == 'mean'):
+                profile_1d[j,i]   = np.nanmean(im[is_good])
+        
+            if (method == 'median'):
+                profile_1d[j,i] = np.nanmedian(im[is_good])
+    
+            clipped = astropy.stats.sigma_clip(im[is_good], sigma=1.5)
+            
+            std_1d[j,i] = np.std(clipped)
+        
+    if do_std: 
+        return (radius_1d, profile_1d, std_1d)
+    
+    return (radius_1d, profile_1d)
+
+### END OF FUNCTION DEFINTION
+    
+if (__name__ == '__main__'):
+    pass
+
+
+
 ### END OF FUNCTION DEFINTION
     
 if (__name__ == '__main__'):
